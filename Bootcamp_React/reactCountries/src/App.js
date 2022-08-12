be12b65322a63e6cf9c4308fa7168cd42c7d9b13
Mariana Correa <mpcs270391@gmail.com>
2022-08-12 09:13:45 -0300

    Finaliza código do projeto

--- Bootcamp_React/reactCountries/src/App.js
+++ Bootcamp_React/reactCountries/src/App.js
@@ -1,21 +1,8 @@
-export default function App() {
-  console.log('Teste no console do navegador')
+import React from "react";
+import ReactCountriesPage from "./pages/ReactCountriesPage.jsx";
 
-  return (
-    <div>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Projeto base para o Módulo React I
-          </h1>
-        </div>
-      </header>
+const App = () => {
+    return <ReactCountriesPage />;
+};
 
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
-    </div>
-  )
-}
+export default App;
